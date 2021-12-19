e02ecc7054a9f7f21cb6128a92521b16c1b04a13
ChrisT-CC <cristaecc@gmail.com>
2021-12-19 20:19:55 +0000

    Add code to pass the test and export the addition function

---
+++ scripts/calculator.js
@@ -0,0 +1,7 @@
+// define addition function with just enough  code to get the test to pass and nothing else
+function addition() {
+    return 42;
+}
+
+// before the test will pass we have to export the function
+module.exports = addition;
