4e9b735515ede1d80dbd542b1f148d02628c628e
ChrisT-CC <cristaecc@gmail.com>
2021-12-20 16:52:32 +0000

    Change the default Node.js environment to jsdom in the test file

--- mocking/scripts/tests/button.test.js
+++ mocking/scripts/tests/button.test.js
@@ -1,3 +1,12 @@
+/* 
+Change the default Node.js environment to jsdom (a browser-like environment, used when building a web app)
+By adding a @jest-environment docblock at the top of the file 
+*/
+
+/**
+ * @jest-environment jsdom
+ */
+
 // import the buttonClick function that we're testing from the button.js file
 const buttonClick = require("../button");
 
