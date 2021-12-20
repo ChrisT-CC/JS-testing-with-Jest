ee25a0ccfeb90d80f6faabc6c8baa278ee245221
ChrisT-CC <cristaecc@gmail.com>
2021-12-20 08:49:06 +0000

    Add code to pass the second test

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,6 +1,6 @@
 // define addition function with just enough  code to get the test to pass and nothing else
-function addition() {
-    return 42;
+function addition(num1, num2) {
+    return num1 + num2;
 }
 
 // before the test will pass we have to export the function
